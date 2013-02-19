c5b685bb1c86b8a2facae68ebaafaae4ada8c99a
iag <maricauxglenn@gmail.com>
2013-02-19 12:50:35 +0100

    Revert "first basic glut program using tutorial: http://www.3dcodingtutorial.com/Your-first-OpenGL-program/The-first-OpenGL-program-with-GLUT.html"
    This reverts commit 098411d86fccb9ed44b9a5e8c4332d14c5bf8c7d.

--- MultimediaOpenGLProject/main.cpp
+++ MultimediaOpenGLProject/main.cpp
@@ -14,42 +14,64 @@
 #include <GL/glut.h> //GLUT bevat OpenGL en GLU
 #endif
 
+#include <stdlib.h>
 
-// ---- Prototypes ----
-void Render(void);
-void Initialise();
-
-// ---- Main Function ----
-int main(int argc, char** argv)
+void init(void)
 {
-	glutInit(&argc,argv);
-	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(600,600);
-	glutInitWindowPosition(0,0);
-	glutCreateWindow("Project Multimedia 6");
-	glutDisplayFunc(Render);
-	Initialise();
-	glutMainLoop();
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_FLAT);
+    //
 }
 
-// ---- Render Function ----
-void Render(void)
+void display(void)
 {
     glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0, 1.0, 1.0);
     
-    glColor3f(1.0f, 1.0f, 1.0f);
-	glBegin(GL_POLYGON);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(-0.5, 0.5);
-    glVertex2f(0.5, 0.5);
-    glVertex2f(0.5, -0.5);
-	glEnd();
+    glLoadIdentity(); //clear matrix
+    gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0); //viewing information
+    glScalef(1.0, 2.0, 1.0);//modelling transformation
+    glutWireCube(1.0); //draw primitive
     
-	glFlush();
+    glFlush();
+    
 }
 
-// ---- Initialise Function ----
-void Initialise()
+void reshape(int w, int h) //function called when window is resized
 {
-	glClearColor (1.0, 0.0, 1.0, 0.0);
+    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void keyboard(unsigned char key, int x, int y)//function called when key is pressed
+{
+    //exit program when exit is pressed (key 27)
+    switch (key) {
+        case 27:
+            exit(0);
+            break;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv); //initialize glut
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);//single buffer, color system (could also be RGBA)
+    
+    glutInitWindowSize(500, 500);//set window size
+    glutInitWindowPosition(100, 100);//set window pos
+    
+    glutCreateWindow(argv[0]);//create window!
+    init();
+    
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    
+    glutMainLoop();
+    return 0;
+    
 }
